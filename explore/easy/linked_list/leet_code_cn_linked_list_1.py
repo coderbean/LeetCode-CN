8380ebe794a78e641a298b2f49bf00f5bde58154
JacobChang <371327964@qq.com>
2018-06-12 19:04:17 +0800

    删除链表中的节点 remove-linked-list-element

---
+++ explore/easy/linked_list/leet_code_cn_linked_list_1.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
+
+if __name__ == '__main__':
+    node1 = ListNode(4)
+    node2 = ListNode(5)
+    node3 = ListNode(1)
+    node4 = ListNode(9)
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    solution = Solution()
+    result = solution.deleteNode(node2)
+    print("DONE")
